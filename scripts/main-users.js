d3f37a89b3e53ca8030468ad4d4fc31cdf4aca0d
2y2son4 <adrianadelteso@gmail.com>
2021-04-06 11:42:48 +0200

    :sparkles: Add more info to user's profile

--- scripts/main-users.js
+++ scripts/main-users.js
@@ -2,12 +2,15 @@
 const btn = document.querySelector('.js-btn2');
 const userElement2 = document.querySelector('.js-user-result2');
 const nameElement2 = document.querySelector('.js-name-result2');
+const locationElement2 = document.querySelector('.js-location-result2');
 const urlElement2 = document.querySelector('.js-url-result2');
 const img2 = document.querySelector('.js-img2');
+const bio = document.querySelector('.js-bio-result2');
 const repoElement2 = document.querySelector('.js-repo-result2');
 const inputElement2 = document.querySelector('.js-input2');
 const followersElement = document.querySelector('.js-followers-result2');
 const followingElement = document.querySelector('.js-following-result2');
+const twitter = document.querySelector('.js-twitter-result2');
 
 const form2 = document.querySelector('.js-form2');
 
@@ -19,14 +22,19 @@ function getGitHubUser() {
     .then((data) => {
       userElement2.innerHTML = data.login + '.';
       nameElement2.innerHTML = data.name + '.';
+      locationElement2.innerHTML = data.location + '.';
       urlElement2.innerHTML = data.html_url;
       urlElement2.href = data.html_url;
       urlElement2.target = '_blank';
       img2.src = data.avatar_url;
       img2.alt = `${data.name}'s avatar`;
+      bio.innerHTML = data.bio;
       repoElement2.innerHTML = data.public_repos + '.';
       followersElement.innerHTML = data.followers + '.';
       followingElement.innerHTML = data.following + '.';
+      twitter.href = 'https://twitter.com/' + data.twitter_username;
+      twitter.innerHTML = '<i class="fab fa-twitter twitter"></i> ';
+      twitter.innerHTML += '@' + data.twitter_username;
       console.log(data);
       // console.log(data.public_repos);
     });
